2289af57685b449470ab800d0a27338e58268757
YoussefMu23 <148534137+YoussefMu23@users.noreply.github.com>
2023-10-24 19:14:54 +0200

    Update DockStation.java

--- TruckDoccking/src/DockStation.java
+++ TruckDoccking/src/DockStation.java
@@ -1,27 +1,30 @@
 public class DockStation {
+
+    // Dockstaion represents station for parking trucks.
     private String stationId;
     private boolean isOccupied;
     private Truck parkedTruck;
 
+    // Constructor
     public DockStation(String id) {
         this.stationId = id;
         this.isOccupied = false;
     }
     public String getStationId() {
-        return stationId;
+        return stationId;            // Get station ID.
     }
 
     public boolean isAvailable() {
-        return !isOccupied;
+        return !isOccupied;          // Check if the station is available and not occupied.
     }
 
     public void park(Truck truck) {
         isOccupied = true;
-        parkedTruck = truck;
+        parkedTruck = truck;          // Parks truck at station and mark it as occupied. 
     }
 
     public Truck getParkedTruck() {
-        return parkedTruck;
+        return parkedTruck;            // Get the truck parked at the station.
     }
 }
 
